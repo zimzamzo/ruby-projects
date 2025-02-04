9609c7f965147a8a888180b6ff4c838c5a82fd93
Alan H <190987223+zimzamzo@users.noreply.github.com>
2025-02-04 13:27:47 -0600

    Create won_round function for player score inc

--- TicTacToe/player.rb
+++ TicTacToe/player.rb
@@ -2,13 +2,14 @@
 #
 # This class handles player-related attributes and actions.
 class Player
-  attr_reader :name
+  attr_reader :name, :score
 
   def initialize(name)
     @name = name
+    @score = 0
+  end
+
+  def won_round
+    @score += 1
   end
 end
-
-# p = Player.new('steve')
-
-# puts "their name is #{p.name}"
