240638cc365f23eab5758c34082a0e1bbfb5a424
Alan H <190987223+zimzamzo@users.noreply.github.com>
2025-02-05 20:47:13 -0600

    Initiall setup, create files

---
+++ Mastermind/lib/code.rb
@@ -0,0 +1,13 @@
+class Code
+  def initialize
+    @code = [nil, nil, nil, nil]
+  end
+
+  def randomize
+  
+  end
+
+  def assign(num1, num2, num3, num4)
+  
+  end
+end
---
+++ Mastermind/lib/game.rb
@@ -0,0 +1,9 @@
+class Game
+  def initialize
+    
+  end
+
+  def play
+
+  end
+end
