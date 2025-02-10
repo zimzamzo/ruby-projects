55993784b1e870c664606359d5f883c6545ad441
Alan H <190987223+zimzamzo@users.noreply.github.com>
2025-02-10 10:38:29 -0600

    Attempt logic for shared value count

--- Mastermind/lib/computer.rb
+++ Mastermind/lib/computer.rb
@@ -7,16 +7,67 @@ class ComputerCodebreaker
   end
 
   def make_guess
-    history = @board.board
+    guess_history = @board.board
     keep_trying = true
-    guess = nil
+    new_guess = nil
     while keep_trying
       new_guess = Array.new(Code::CODE_LENGTH).map { |_| Code::CODE_VALUES.sample }
-      history.all? { |old_guess| match_feedback?(old_guess, new_guess) }
-      keep_trying = false
+      keep_trying = false if guess_history.all? do |old_guess| 
+                               match_feedback?(old_guess, new_guess)
+                             end
     end
-    guess
+    new_guess
+  end
+
+  def match_feedback?(old_info, new_code)
+    old_code = old_info[0].code
+    old_positions_correct = old_info[1]
+    old_values_correct = old_info[2]
+
+    shares_positions(old_code, new_code) == old_positions_correct &&
+      shares_values(old_code, new_code) == old_values_correct
+  end
+
+  def shares_positions(first, second)
+    first.each_with_index.reduce(0) do |acc, (val, ind)|
+      acc + (val == second[ind] ? 1 : 0)
+    end
+  end
+
+  def shares_values(first, second)
+
   end
 end
 
 puts ComputerCodebreaker.new.make_guess
+
+
+# 
+# secret ccda
+# guess 
+# abcd 0 3 (A)
+# aadd 1 1
+# bcbd 1 1
+# bacc 0 3
+# acac 1 2
+# 
+# freq secret 1 0 2 1
+# freq guesses
+# 1 1 1 1
+# 2 0 0 2
+# 0 2 1 1
+# 1 1 2 0
+# 2 0 2 0
+# 
+# guess def 
+# A1 1 -1 -1 1
+# A2 -1 1 1 -1
+# A3 0 0 1 -1
+# A4 
+# 
+# remove 0s
+#  1 1
+#  1 0 0
+#  0 0 1
+#  1 1
+# 
