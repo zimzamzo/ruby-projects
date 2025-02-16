f4cb718489c52a8a748679ee51a688fc34f25ec3
Alan H <190987223+zimzamzo@users.noreply.github.com>
2025-02-16 13:35:02 -0600

    Initialize hangman

---
+++ Hangman/lib/hangman.rb
@@ -0,0 +1,13 @@
+# run the game
+
+class Hangman
+  attr_accessor :secret_word
+  
+  def initialize
+    
+  end
+
+  def play
+    puts 'started'
+  end
+end
---
+++ Hangman/main.rb
@@ -0,0 +1,5 @@
+# main
+
+require_relative 'lib/hangman'
+
+Hangman.new.play
